85cf1d1422189aab8a78315ce07ec4a0278182b3
Alan Williams <astropiloto@gmail.com>
2024-07-02 13:30:03 -0500

    made another CI exlusion for annotation and updated CI to use latest R

--- ggsashimi.py
+++ ggsashimi.py
@@ -1059,9 +1059,10 @@ if __name__ == "__main__":
         else:
             plot(R_script)
         new_annotation = []
-        for exon in annotation["exons"]['"NM_001040108.2"']:
-            new_exon = (int(exon[0]) + 1, int(exon[1]) + 1)
-            new_annotation.append(new_exon)
+        if '"NM_001040108.2"' in annotation["exons"]:
+            for exon in annotation["exons"]['"NM_001040108.2"']:
+                new_exon = (int(exon[0]) + 1, int(exon[1]) + 1)
+                new_annotation.append(new_exon)
         annotation_lookup = {}
         exon_count = len(new_annotation)
         for exon in new_annotation:
